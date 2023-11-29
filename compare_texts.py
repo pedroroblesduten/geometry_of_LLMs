4867ed704e4d28c2640a851c29100908b6c837c9
pedroroblesduten <pedroroblesduten@gmail.com>
2023-11-29 19:12:40 -0300

    a

--- compare_texts.py
+++ compare_texts.py
@@ -21,6 +21,7 @@ class CompareText:
             print(f"Error: File {file_path} not accessible or does not exist.")
             return ""
 
+
     def plot_convex_hull_comparison(self, file_path1, file_path2):
         text1 = self.read_text(file_path1)
         embeddings1 = self.model_llm.get_embeddings_from_input(text1)[0]
@@ -28,37 +29,35 @@ class CompareText:
         text2 = self.read_text(file_path2)
         embeddings2 = self.model_llm.get_embeddings_from_input(text2)[0]
 
-        # Determine suitable perplexity values
-        perplexity1 = min(30, len(embeddings1) - 1)
-        perplexity2 = min(30, len(embeddings2) - 1)
+        # Combine embeddings for t-SNE
+        combined_embeddings = np.vstack((embeddings1, embeddings2))
 
+        # Apply t-SNE to the combined embeddings
         tsne = TSNE(n_components=2, random_state=0)
+        reduced_embeddings_combined = tsne.fit_transform(combined_embeddings)
 
-        # Apply t-SNE with adjusted perplexity for each set of embeddings
-        reduced_embeddings1 = tsne.set_params(perplexity=perplexity1).fit_transform(embeddings1)
-        reduced_embeddings2 = tsne.set_params(perplexity=perplexity2).fit_transform(embeddings2)
-
-        # Compute the convex hulls
+        # Split the embeddings back into their respective sets
+        reduced_embeddings1 = reduced_embeddings_combined[:len(embeddings1)]
+        reduced_embeddings2 = reduced_embeddings_combined[len(embeddings1):]
+        # Compute the convex hulls and plot as before
         hull1 = ConvexHull(reduced_embeddings1)
         hull2 = ConvexHull(reduced_embeddings2)
 
-        # Plot the embeddings and convex hulls
         plt.scatter(reduced_embeddings1[:, 0], reduced_embeddings1[:, 1], alpha=0.7, color='blue', label='Text 1: Original')
         plt.scatter(reduced_embeddings2[:, 0], reduced_embeddings2[:, 1], alpha=0.7, color='green', label='Text 2: Generated')
 
         for simplex in hull1.simplices:
             plt.plot(reduced_embeddings1[simplex, 0], reduced_embeddings1[simplex, 1], 'k-')
-
         for simplex in hull2.simplices:
             plt.plot(reduced_embeddings2[simplex, 0], reduced_embeddings2[simplex, 1], 'k-')
 
-        # Additional plot settings
         plt.title('t-SNE Convex Hull Comparison')
         plt.xlabel('t-SNE Dimension 1')
         plt.ylabel('t-SNE Dimension 2')
         plt.legend()
         plt.savefig('./results/tsne_compare.png')
-        print('plot salvo')
+        print('plot saved')
+
 
     def compare_geometric_metrics(self, file_path1, file_path2):
         text1 = self.read_text(file_path1)
