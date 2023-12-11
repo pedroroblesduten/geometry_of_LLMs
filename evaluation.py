e5901a4b1bcf46244a71a1e0a3e333da280d8551
Henrique Hott <henrique.hott1996@gmail.com>
2023-12-11 20:00:37 -0300

    Refactor metrics and model WIP

---
+++ evaluation.py
@@ -0,0 +1,25 @@
+from umap import UMAP
+from scipy.spatial import ConvexHull
+from typing import *
+
+def compute_geometric_metrics(
+    self, 
+    embeddings, 
+    dimensionality_reduction_model=UMAP(n_components=9)
+    ) -> Dict[str, Any]:
+
+    if embeddings.ndim > 2:
+        embeddings = embeddings.reshape(-1, embeddings.shape[-1]) 
+
+    centroid = np.mean(embeddings, axis=0)
+    distances = cdist(embeddings, [centroid], 'euclidean')
+
+    reduced_embeddings = dimensionality_reduction_model.fit_transform(embeddings)
+    
+    # Compute the convex hull
+    hull = ConvexHull(reduced_embeddings)
+
+    volume = hull.volume
+    area = hull.area
+
+    return {"mean_distance": np.mean(distances), "volume": hull.volume, "area": hull.area}
--- model_llm.py
+++ model_llm.py
@@ -2,61 +2,60 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import h5py
 import numpy as np
+from typing import *
+
 
 class ModelLLM:
     def __init__(self,
-                 model_name,
-                 save_results_path
-                 ):
+                 model_name_or_path,
+                 results_path,
+                 device,
+                 torch_dtype=torch.float16
+        ):
 
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
-        self.model = AutoModelForCausalLM.from_pretrained(model_name, device_map='auto', torch_dtype=torch.float16)
+        self.device = device
+        self.torch_dtype = torch_dtype
+        self.model_name_or_path = model_name_or_path
+        self.results_path = results_path
+        self.__load_checkpoint()
+        
+
+
+    def __load_checkpoint(self) -> None:
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_or_path)
+        self.model = AutoModelForCausalLM.from_pretrained(
+            self.model_name_or_path, 
+            device_map=self.device, 
+            torch_dtype=self.torch_dtype
+        )
+        
         self.model.eval()
-        self.save_results_path = save_results_path
 
-    def generate_text(self, input_prompt, max_new_tokens=30):
+    def generate(self, input_prompt, max_new_tokens=30, return_embbdings=True)-> Tuple[Any, Any]:
         model_input = self.tokenizer(input_prompt, return_tensors="pt").to("cuda")
         input_length = model_input.input_ids.size(1)
 
         with torch.no_grad():
-            generated_output = self.model.generate(**model_input, max_new_tokens=max_new_tokens)
-            generated_text = self.tokenizer.decode(generated_output[0][input_length:], skip_special_tokens=True)
-        return generated_text
+            generated_output = self.model.generate(
+                **model_input, max_new_tokens=max_new_tokens)
 
-    def generate_tokens(self, text):
-        tokens = self.tokenizer(text, return_tensors="pt").input_ids
+            generated_text = self.tokenizer.decode(
+                generated_output[0][input_length:], skip_special_tokens=True)
+        
+        return generated_text, generated_output if return_embbdings else generated_text
+
+    def generate_tokens(self, text)-> torch.Tensor:
+        tokens = self.tokenizer(text, return_tensors="pt")
         return tokens
 
-    def generate_embeddings(self, text):
-        model_input = self.tokenizer(text, return_tensors="pt").to("cuda")
-        with torch.no_grad():
-            outputs = self.model(**model_input, output_hidden_states=True)
-            embeddings = outputs.hidden_states[-1].cpu().numpy()
-        return embeddings
-
-    def get_embeddings_from_input(self, input_text):
+    def get_embeddings_from_input(self, input_text)-> torch.Tensor:
         model_input = self.tokenizer(input_text, return_tensors="pt").to("cuda")
         with torch.no_grad():
             outputs = self.model(**model_input, output_hidden_states=True)
-            input_embeddings = outputs.hidden_states[-1].cpu().numpy()
+            input_embeddings = outputs.hidden_states[-1].cpu()
+        
         return input_embeddings
 
     def save_results(self, file_name, generated_text, generated_tokens, generated_embeddings):
-        with h5py.File(self.save_results_path + file_name, 'w') as f:
-            f.create_dataset('generated_text', data=np.string_(generated_text))
-            f.create_dataset('tokens', data=generated_tokens.cpu().numpy())
-            f.create_dataset('embeddings', data=generated_embeddings)
-
-# Example usage
-model_llm = ModelLLM("meta-llama/Llama-2-7b-chat-hf", "./results/")
-input_text = "Complete o texto a seguir: tenho por dom a paixao, nas queimadas de tronco seco"
-text = model_llm.generate_text(input_text)
-print(text)
-input_embeddings = model_llm.get_embeddings_from_input(input_text)
-print(input_embeddings.shape)
-
-
-model.generate_text('oi tudo bem')
-
-model.plot_convex_hull(text1=caminho.txt)
+        raise NotImplementedError("")
 
